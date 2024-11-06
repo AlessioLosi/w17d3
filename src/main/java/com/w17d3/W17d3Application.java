448539eae91ce074f572b04e1d0df73dc7ff3f72
AlessioLosi <alessiolosi93@icloud.com>
2024-11-06 14:54:11 +0100

    start

---
+++ src/main/java/com/w17d3/W17d3Application.java
@@ -0,0 +1,13 @@
+package com.w17d3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W17d3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W17d3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/w17d3/W17d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w17d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W17d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
